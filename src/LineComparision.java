51262fb9ccd22cf4dd09690d0f1031556aa2d50f
Surya2742 <boby7724284@gmail.com>
2022-08-05 10:56:40 +0530

    added: CoOrdinates and Length of Line

--- src/LineComparision.java
+++ src/LineComparision.java
@@ -1,14 +1,11 @@
 public class LineComparision {
-
     double coOrdinates (int x1, int x2, int y1, int y2) {
         double lengthFormula = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
         return lengthFormula;
     }
-
     public static void main(String[] args) {
         LineComparision calculation = new LineComparision();
         double length = calculation.coOrdinates(1, 5, 3, 4);
-        System.out.println("Distance between two point is: " + length);
+        System.out.println("Distance between two point is : " + length);
     }
-
 }
